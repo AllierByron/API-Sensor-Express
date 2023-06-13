d5c15cbe34556d9f3ce8572f9453f8b04ab133d6
Byron <212220367@soy.utj.edu.mx>
2023-06-13 14:29:07 -0600

    Cambios en metodo POST y creacion de uno nuevo

--- routes/sensorAPI.js
+++ routes/sensorAPI.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+//variables mias
+var activo;
+var codigoDoc;
+
 /* GET users listing. */
 router.post('/', function(req, res) {
-//   res.status(201).send('temperatura:'+req.body.temperatura+"\n"+    
-//             'humedad: '+req.body.humedad);
-    res.send("si jala");
+    res.status(201).send('temperatura:'+req.body.temperatura+"\n"+    
+            'humedad: '+req.body.humedad);
+    // res.send("si jala");
 });
+
+//este metodo get sirve para decir que cierto ticket esta activo 
+//para registrar informacion del sensor
+//lo que hace es devolver informacion del ticket, y las registra en las
+//varibles que estan arriba
+router.post('/enviar', function(req, res) {
+    
+});
+
 
 module.exports = router;
